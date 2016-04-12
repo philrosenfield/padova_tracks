b4862432c1084400d524c375af225f45bdce3b0c
Phil Rosenfield <philrosenfield@gmail.com>
2016-04-12 12:22:29 -0400

    fixing for merge

--- tracks/track.py
+++ tracks/track.py
@@ -178,10 +178,6 @@ class Track(object):
             except:
                 pass
 
-<<<<<<< HEAD
-=======
-        
->>>>>>> c95842bdd96d8ee1103e9e5b3a78ed5a785f30ea
         if hasattr(self, 'data') and hasattr(self.data, 'QHEL'):
             if self.hb:
                 self.zahb_mcore = self.data.QHEL[0]
