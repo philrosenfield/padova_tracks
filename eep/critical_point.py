c95842bdd96d8ee1103e9e5b3a78ed5a785f30ea
Phil Rosenfield <philrosenfield@gmail.com>
2016-04-12 12:15:50 -0400

    fixed ptcri reader when called by Track

--- eep/critical_point.py
+++ eep/critical_point.py
@@ -4,6 +4,7 @@ import numpy as np
 import sys
 
 import logging
+from ..config import *
 logger = logging.getLogger()
 
 class Eep(object):
@@ -73,7 +74,10 @@ class critical_point(object):
     '''
     def __init__(self, filename=None, sandro=True, debug=False, hb=False):
         self.debug = debug
+        
         if filename is not None:
+            if 'hb' in filename:
+                hb = True
             self.base, self.name = os.path.split(filename)
             self.load_ptcri(filename, sandro=sandro, hb=hb)
             self.get_args_from_name(filename)
@@ -162,7 +166,7 @@ class critical_point(object):
         # the lines have the path name, and the path has F7.
         if not hb:
             begin, = [i for i in range(len(lines)) if lines[i].startswith('#')
-                    and 'F7' in lines[i]]
+                      and 'F7' in lines[i]]
         else:
             if 'p2m' in filename:
                 begin = 0
